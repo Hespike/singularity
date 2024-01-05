4d8f69c7b46ca6e1ea308a40210e7f31d2eb20c0
Sárdi András <sardi.andras@gmail.com>
2024-01-05 18:58:03 +0100

    .

---
+++ scripts.js
@@ -0,0 +1,28 @@
+document.getElementById("strongerpunchButton").addEventListener("click", function() {
+    document.getElementById("textarea").innerHTML = "Your punches are stronger. Your blows deal one more damage dice (1d4 instead of 1d2 by default).";
+  });
+
+  document.getElementById("strongerbodyButton").addEventListener("click", function() {
+    document.getElementById("textarea").innerHTML = "You are stronger against attacks. Your defensive prowess increases by 2.";
+    generateTableData();
+  });
+
+
+  function generateTableData() {
+    const table = document.getElementById('myTable');
+    const levelData = [1, 5, 10, 15, 20];
+    const bonusData = [2, 4, 6, 8, 10];
+  
+    for (let i = 0; i < levelData.length; i++) {
+      const tr = document.createElement('tr');
+      const levelTd = document.createElement('td');
+      const bonusTd = document.createElement('td');
+  
+      levelTd.textContent = levelData[i];
+      bonusTd.textContent = bonusData[i];
+  
+      tr.appendChild(levelTd);
+      tr.appendChild(bonusTd);
+      table.appendChild(tr);
+    }
+  }
